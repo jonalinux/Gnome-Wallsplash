fa09c1482483a57735be12b31677e1b7f5336ad3
Jonathan Sanfilippo <103053714+JonathanSanfilippo@users.noreply.github.com>
2022-07-24 20:37:19 +0100

    Delete fix directory

--- fix/fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Author Jonathan Sanfilippo
-
-
-
-cp ~/.Wallsplash/asset/background  ~/.Wallsplash/data/
-mv ~/.Wallsplash/data/background ~/.Wallsplash/data/wallsplash.jpg 
-XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -u normal -i ~/.Wallsplash/asset/icon.svg "Wallsplash" "I couldn't download the Wallpaper, I set the temporary one :(" 
